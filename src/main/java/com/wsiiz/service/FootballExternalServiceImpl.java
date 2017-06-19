827f96b379343c85ad220c51fc4debfdc1d053ff
w53387 <w53387@student.wsiz.rzeszow.pl>
2017-06-19 20:13:55 +0200

    Tournament parser finished

--- src/main/java/com/wsiiz/service/FootballExternalServiceImpl.java
+++ src/main/java/com/wsiiz/service/FootballExternalServiceImpl.java
@@ -1,7 +1,11 @@
 package com.wsiiz.service;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.wsiiz.dao.TeamDao;
+import com.wsiiz.dao.TeamDaoImpl;
+import com.wsiiz.domain.Team;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,6 +13,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,36 +22,51 @@ import org.springframework.stereotype.Service;
 @Service
 public class FootballExternalServiceImpl implements FootballExternalService {
 
+  @Autowired
+  private TeamDaoImpl teamDao;
+
   @Override
   public void getCompetition(int season) throws MalformedURLException {
     URL url = new URL("http://api.football-data.org/v1/competitions/?season" + season);
     JsonObject jsonObject = performRequest(url);
-    System.out.println(jsonObject);
+
+    System.out.println("GET COMPETITION " + jsonObject);
   }
 
   @Override public void getTeam(int competitonId) throws MalformedURLException  {
     URL url = new URL("http://api.football-data.org/v1/competitions/398/teams");
     JsonObject jsonObject = performRequest(url);
-    System.out.println(jsonObject);
+    System.out.println("GET TEAM " + jsonObject);
+    JsonArray teams = jsonObject.get("teams").getAsJsonArray();
+
+    for (int i = 0; i < teams.size(); i++) {
+      JsonObject singleTeam = teams.get(i).getAsJsonObject();
+      Team team = new Team();
+      team.setName(singleTeam.get("name").getAsString());
+      team.setMarketValue("undef");
+      team.setCrestUrl(singleTeam.get("crestUrl").getAsString());
+      teamDao.insert(team);
+    }
+    System.out.println("GET TEAM ARRAY OF TEAMS: " + teams);
   }
 
   @Override public void getLegueTable(int competitonId) throws MalformedURLException  {
     URL url = new URL("http://api.football-data.org/v1/competitions/" + competitonId + "/leagueTable");
     JsonObject jsonObject = performRequest(url);
-    System.out.println(jsonObject);
+    System.out.println("GET Table " + jsonObject);
   }
 
   @Override public void getFixtures(int competitonId, int matchDay) throws MalformedURLException {
     URL url = new URL(" http://api.football-data.org/v1/competitions/"+
         competitonId+ "/fixtures?matchday="+ matchDay);
     JsonObject jsonObject = performRequest(url);
-    System.out.println(jsonObject);
+    System.out.println("GET fix " + jsonObject);
   }
 
   @Override public void headToHead(int fixtureId) throws MalformedURLException {
     URL url = new URL("http://api.football-data.org/v1/fixtures/" + fixtureId);
     JsonObject jsonObject = performRequest(url);
-    System.out.println(jsonObject);
+    System.out.println("GET HtH " + jsonObject);
   }
 
   private JsonObject performRequest (URL url) {
@@ -77,5 +97,13 @@ public class FootballExternalServiceImpl implements FootballExternalService {
 
     }
     return null;
+  }
+
+  public void setTeamDao(TeamDaoImpl teamDao) {
+    this.teamDao = teamDao;
+  }
+
+  public TeamDaoImpl getTeamDao() {
+    return teamDao;
   }
 }
